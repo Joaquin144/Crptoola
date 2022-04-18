8915cba67b36e28e924b8133ce930954e1298f56
Joaquin144 <vvpp898.comco@gmail.com>
2022-04-18 16:30:40 +0530

    app finished

--- app/src/main/java/com/devcommop/joaquin/crptoola/domain/use_case/get_coin/GetCoinUseCase.kt
+++ app/src/main/java/com/devcommop/joaquin/crptoola/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -16,13 +16,13 @@ class GetCoinUseCase @Inject constructor(
     //Flow tab use karte hain jab ek function time vary karne par different types ki values ko emit karega. Eg: start mei loading phir success ya error and phir user ne kuch change kiya toh dobara loading phir success ya error
     operator fun invoke(coinId: String) : Flow<Resource<CoinDetail>> = flow {
         try{
-            emit(Resource.Loading())
+            emit(Resource.Loading<CoinDetail>())
             val coin = repository.getCoinById(coinId).toCoinDetail()//E! => why this : yeh toh khaali hai 😑
-            emit(Resource.Success(coin))
+            emit(Resource.Success<CoinDetail>(coin))
         }catch (e: HttpException){//response of server isn't a success (success => codestarts with 2)
-            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected error occurred"))
         }catch(e : IOException){//when repo can't talk to server eg no internet connection or server is offline
-            emit(Resource.Error(e.localizedMessage ?: "Couldn't reach server. Check your Internet connection"))
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "Couldn't reach server. Check your Internet connection"))
         }
     }
 }
--- app/src/main/java/com/devcommop/joaquin/crptoola/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ app/src/main/java/com/devcommop/joaquin/crptoola/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -16,13 +16,13 @@ class GetCoinsUseCase @Inject constructor(
     //Flow tab use karte hain jab ek function time vary karne par different types ki values ko emit karega. Eg: start mei loading phir success ya error and phir user ne kuch change kiya toh dobara loading phir success ya error
     operator fun invoke() : Flow<Resource<List<Coin>>> = flow {
         try{
-            emit(Resource.Loading())
+            emit(Resource.Loading<List<Coin>>())
             val coins = repository.getCoins().map{ it.toCoin() }//E! => why this : yeh toh khaali hai 😑
-            emit(Resource.Success(coins))
+            emit(Resource.Success<List<Coin>>(coins))
         }catch (e: HttpException){//response of server isn't a success (success => codestarts with 2)
-            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occurred"))
         }catch(e : IOException){//when repo can't talk to server eg no internet connection or server is offline
-            emit(Resource.Error(e.localizedMessage ?: "Couldn't reach server. Check your Internet connection"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "Couldn't reach server. Check your Internet connection"))
         }
     }
 }
